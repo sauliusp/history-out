8c9190604dd42551549eb91779fc8e3be527b359
Saulius Petreikis <sauliuspetr@gmail.com>
2025-02-01 12:28:12 +0200

    leaving source maps only upon dev command

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'extension-unpacked'),
-    devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            compilerOptions: {
-              sourceMap: true,
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'extension-unpacked'),
+      devtoolModuleFilenameTemplate: isDevelopment
+        ? 'webpack:///[absolute-resource-path]'
+        : undefined,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                sourceMap: isDevelopment,
+              },
             },
           },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  devtool: 'source-map',
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: isDevelopment ? 'source-map' : false,
+  };
 };
