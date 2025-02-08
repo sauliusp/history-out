b471e38f7ff2cf3c50c8398b0e9b5f51769022e5
Saulius Petreikis <sauliuspetr@gmail.com>
2025-02-08 13:36:22 +0200

    refactor: improve extension initialization and dependencies
    - Move UI packages to devDependencies since they're bundled
    - Wrap sidePanel initialization in onInstalled listener
    - Add sidePanel API availability check

--- extension-unpacked/background.js
+++ extension-unpacked/background.js
@@ -1,3 +1,9 @@
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+chrome.runtime.onInstalled.addListener(() => {
+  if (chrome.sidePanel) {
+    chrome.sidePanel
+      .setPanelBehavior({ openPanelOnActionClick: true })
+      .catch((error) => console.error(error));
+  } else {
+    console.error('SidePanel API not available');
+  }
+});
